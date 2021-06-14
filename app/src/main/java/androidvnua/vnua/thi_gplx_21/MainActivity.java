199ca488482628f07f916becb1215bb2fa80a8fa
duykhanh <zerok49tt@gmail.com>
2021-06-14 19:33:46 +0700

    splash screen

--- app/src/main/java/androidvnua/vnua/thi_gplx_21/MainActivity.java
+++ app/src/main/java/androidvnua/vnua/thi_gplx_21/MainActivity.java
@@ -10,34 +10,10 @@ import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-//    private static int SPLASH_SCREEN = 1;
-//
-//    Animation animAlpha;
-//    TextView logo;
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-
         Intent intent = new Intent(MainActivity.this, Home.class);
         startActivity(intent);
         finish();
-
-//        animAlpha = AnimationUtils.loadAnimation(MainActivity.this, R.anim.anim_alpha);
-//
-//        logo = findViewById(R.id.logo);
-//
-//        logo.setAnimation(animAlpha);
-//
-//        new Handler().postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                Intent intent = new Intent(MainActivity.this, Home.class);
-//                startActivity(intent);
-//                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-//                finish();
-//            }
-//        }, SPLASH_SCREEN);
     }
 }
